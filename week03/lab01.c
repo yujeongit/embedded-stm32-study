0bf4e2476c75f9197f408b9646d7910198bd4db7
Yujeong Lee <144709782+yujeongit@users.noreply.github.com>
2026-09-21 11:21:43 +0900

    Add lab01

---
+++ week03/lab01.c
@@ -0,0 +1,32 @@
+#include <stdint.h>
+
+#if !defined(__SOFT_FP__) && defined(__ARM_FP)
+  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#endif
+
+int main(void)
+{
+    int i;
+
+    volatile uint32_t *gpioc_crh=(uint32_t *)(0x40011000+0x04);
+    volatile uint32_t *gpioc_odr=(uint32_t *)(0x40011000+0x0c);
+
+    /* System initialization */
+    *((volatile uint32_t *)(0x40021000)) |= 0x00000001;
+    *((volatile uint32_t *)(0x40021004)) &= 0xF8FF0000;
+    *((volatile uint32_t *)(0x40021000)) &= 0xFEF6FFFF;
+    *((volatile uint32_t *)(0x40021000)) &= 0xFFFBFFFF;
+    *((volatile uint32_t *)(0x40021004)) &= 0xFF80FFFF;
+    *((volatile uint32_t *)(0x40021008))  = 0x009F0000;
+    *((volatile uint32_t *)(0xE000ED08))  = 0x08000000;
+
+    *gpioc_crh = 0x30000;
+
+    *gpioc_odr = 0x1000;
+
+    /* Loop forever */
+    for(;;)
+        ;
+
+    return 0;
+}
